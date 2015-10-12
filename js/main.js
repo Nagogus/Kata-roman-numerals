1ac16725dc63f47aee10e7b28aeaa2da50a82686
Nagogus <nagoga.ruben@gmail.com>
2015-10-12 21:58:09 +0200

    Prettfying

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 
 function convert(number) {
-  var result = [];
+  var result = [],
+      arabicPoints = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+      romanPoints = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'],
+      arabicPointsLength = arabicPoints.length;
 
   if (number === 0 ) {
     return 'nulla';
   }
 
-  var arabicPoints = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  var romanPoints = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
-
-  while(number > 0) {
-    for(var i = 0; i < arabicPoints.length; i++) {
+  while(number) {
+    for(var i = 0; i < arabicPointsLength; i++) {
       if (number >= arabicPoints[i]) {
         number -= arabicPoints[i];
         result.push(romanPoints[i]);
